d9413b71509157009abea231448042e0e2cfaf22
vagrant <vagrant@precise64.(none)>
2014-02-04 17:15:26 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fbchat::Application.config.secret_key_base = '126f8852ed338ba0da8c26d1f19f4bda3eab93efe2da93f4485e250e014d4f67e1aadd1fd240e40856ca4f4bb880d875331e9bc57b55ea9c5e28786a852277a3'
